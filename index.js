fb65202a7df48ed5c69964615610188e4defbbdd
Ubuntu64 <ybb_y1b1b1@163.com>
2018-02-16 00:32:31 -0800

    add main.html

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var app = express();
 
 app.use(express.static('public'));
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  //res.send('Hello World!');
+  res.redirect('/main.html');
 });
 
 var server = app.listen(3000, function () {
