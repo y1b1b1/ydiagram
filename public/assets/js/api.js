7b3333af5b139c1613cc2a23dd613bef818c1fee
Ubuntu64 <ybb_y1b1b1@163.com>
2018-02-17 01:26:34 -0800

    remove google-analysis call

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -206,6 +206,7 @@ function initSearch () {
 }
 $(document).ready(initSearch);
 
+/*
 (function (i, s, o, g, r, a, m) {
 i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
   (i[r].q = i[r].q || []).push(arguments)
@@ -215,3 +216,4 @@ i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
 
 ga('create', 'UA-1506307-5', 'auto');
 ga('send', 'pageview');
+*/
--- public/assets/js/goDoc.js
+++ public/assets/js/goDoc.js
@@ -34,7 +34,7 @@ function _traverseDOM(node) {
     _traverseDOM(node.childNodes[i]);
   }
 }
-
+/*
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -42,3 +42,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-1506307-5', 'auto');
 ga('send', 'pageview');
+*/
